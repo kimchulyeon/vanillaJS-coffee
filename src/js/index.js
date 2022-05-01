9df38ae1cc7655034bf958dfbed58a5b382f37e5
kimchulyeon <guinness987@gmail.com>
2022-05-02 07:58:08 +0900

    연습: 자바스크립트 다시 짜보기

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,15 @@
 const app = () => {
-  const $ = (element) => document.querySelector(element)
-
-  // 메뉴 추가 입력
-  const inputEl = $('#espresso-menu-name')
+  const $ = (el) => document.querySelector(el)
+  const inputEl = $('.input-field')
   const formEl = $('#espresso-menu-form')
   const ulEl = $('#espresso-menu-list')
-  const confirmBtn = $('.input-submit')
-  const menuCount = $('.menu-count')
+  const addBtn = $('.input-submit')
 
-  // 메뉴 갯수 업데이터 (중복 제거 함수)
-  const updataMenuCount = () => {
-    const count = ulEl.querySelectorAll('li').length
-    menuCount.innerText = `총 ${count}개` // 총 갯수 표시
-  }
-  // 메뉴 추가 과정 (중복 제거 함수)
-  const menuAddProcess = () => {
-    const menuName = inputEl.value
-
-    const menuItemListTemplate = (item) => {
-      return `<li class='menu-list d-flex items-center py-2'>
-      <span class='w-100 pl-2 menu-name'>${item}</span>
-      <button
+  const menuItem = (el) => {
+    return `
+    <li class='menu-list d-flex items-center py-2'>
+      <span class='w-100 pl-2 menu-name'>${el}</span>
+      <button 
         type='button'
         class='bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button'>
           수정
@@ -30,57 +19,30 @@ const app = () => {
         class='bg-gray-50 text-gray-500 text-sm menu-remove-button'>
           삭제
       </button>
-      </li>`
-    }
-
-    ulEl.insertAdjacentHTML('beforeend', menuItemListTemplate(menuName)) // insertAdjacentHTML도 가능
-    updataMenuCount()
-    inputEl.value = '' // Input 빈칸으로 되돌리기
+    </li>`
   }
-  // 수정버튼
-  const editMenuName = (e) => {
-    if (e.target.className.includes('menu-edit-button')) {
-      const MENU_NAME = e.target.closest('li').querySelector('.menu-name')
-      let newMenu = prompt('메뉴명을 수정하세요', MENU_NAME.innerText)
-      MENU_NAME.innerText = newMenu
-    }
-  }
-  // 삭제버튼
-  const removeMenuName = (e) => {
-    if (e.target.className.includes('menu-remove-button')) {
-      const okDelete = confirm('메뉴를 삭제하시겠습니까?')
-      if (okDelete) {
-        // e.target.closest('li').style.display = 'none'
-        e.target.closest('li').remove()
-        updataMenuCount()
-      }
+  const addMenuProcess = () => {
+    if (inputEl.value > 0) {
+      ulEl.insertAdjacentHTML('beforeend', menuItem(inputEl.value))
+      inputEl.value = ''
     }
   }
 
-  // 페이지 새로고침 막기
   formEl.addEventListener('submit', (e) => {
     e.preventDefault()
   })
 
-  // Enter 눌렀을 때 이벤트
+  // ENTER 메뉴 추가
   inputEl.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter' && inputEl.value.length > 0) {
-      menuAddProcess()
+    if (e.key === 'Enter') {
+      addMenuProcess()
     }
   })
 
-  // 확인 버튼 눌렀을 때 이벤트
-  confirmBtn.addEventListener('click', () => {
-    if (inputEl.value > 0) {
-      menuAddProcess()
-    }
-  })
+  // CLICK 메뉴 추가
+  addBtn.addEventListener('click', addMenuProcess)
 
-  // 수정 삭제 버튼 (이벤트 위임)
-  ulEl.addEventListener('click', (e) => {
-    editMenuName(e)
-    removeMenuName(e)
-  })
+  // 삭제 수정 버튼
 }
 
 app()
